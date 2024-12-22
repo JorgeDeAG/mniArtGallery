3d63253bf6c9e256ddfc68f6b4e7e9d186d9e2b4
JorgeDeAG <github@deaguinaga.es>
2024-12-22 22:23:51 +0100

    Delete index.php

--- index.php
+++
@@ -1,3 +0,0 @@
-<html lang="es" dir="ltr"><!-- Mirrored from localhost:9966/ by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 29 Aug 2023 17:39:19 GMT --><head><title>María Nedelkova Ivanova</title><meta charset="utf-8"></head><body style="margin: 0px; padding: 0px;"><script src="index.js"></script><canvas width="1035" height="933" style="border: 0px; margin: 0px; padding: 0px; top: 0px; left: 0px; position: absolute; width: 1035px; height: 933px;"></canvas>
-
-</body></html>
